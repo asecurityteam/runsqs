60bebdcdbaef9f5f0a63e13b24788240d7fac672
Derek Duncan <dduncan@atlassian.com>
2022-08-31 09:45:59 -0500

    added tests to assert length of failed

--- sqsproducer_test.go
+++ sqsproducer_test.go
@@ -57,8 +57,9 @@ func TestDefaultSQSProducer_BatchProduceMessage_Success(t *testing.T) {
 	mockQueue.EXPECT().SendMessageBatch(&sqs.SendMessageBatchInput{
 		QueueUrl: aws.String(producer.QueueURL()),
 	}).Return(&sqs.SendMessageBatchOutput{}, nil)
-	_, err := producer.BatchProduceMessage(context.Background(), sqsBatchMessageInput)
+	resp, err := producer.BatchProduceMessage(context.Background(), sqsBatchMessageInput)
 	assert.Nil(t, err)
+	assert.Len(t, resp.Failed, 0)
 }
 
 func TestDefaultSQSProducer_BatchProduceMessage_Failure(t *testing.T) {
@@ -71,6 +72,26 @@ func TestDefaultSQSProducer_BatchProduceMessage_Failure(t *testing.T) {
 	mockQueue.EXPECT().SendMessageBatch(&sqs.SendMessageBatchInput{
 		QueueUrl: aws.String(producer.QueueURL()),
 	}).Return(&sqs.SendMessageBatchOutput{}, errors.New("error"))
-	_, err := producer.BatchProduceMessage(context.Background(), sqsBatchMessageInput)
+	resp, err := producer.BatchProduceMessage(context.Background(), sqsBatchMessageInput)
 	assert.NotNil(t, err)
+	assert.Len(t, resp.Failed, 0)
+}
+
+func TestDefaultSQSProducer_BatchProduceMessage_Success_Failed_Items(t *testing.T) {
+	var ctrl = gomock.NewController(t)
+	defer ctrl.Finish()
+	mockQueue := NewMockSQSAPI(ctrl)
+	producer := NewDefaultSQSProducer(mockQueue, "www.queueurl.com")
+
+	sqsBatchMessageInput := &sqs.SendMessageBatchInput{}
+	mockQueue.EXPECT().SendMessageBatch(&sqs.SendMessageBatchInput{
+		QueueUrl: aws.String(producer.QueueURL()),
+	}).Return(&sqs.SendMessageBatchOutput{
+		Failed: []*sqs.BatchResultErrorEntry{
+			{},
+		},
+	}, nil)
+	resp, err := producer.BatchProduceMessage(context.Background(), sqsBatchMessageInput)
+	assert.Nil(t, err)
+	assert.Len(t, resp.Failed, 1)
 }
